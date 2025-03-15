97a2082f70daf61571b12ac96c1ce5e6f6222dae
ChaficAchour <shafi2fawaz2018@gmail.com>
2025-03-15 22:45:34 +0200

    some updates ot that import

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->append([
+            \App\Http\Middleware\Cors::class,
+        ]);
+
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
